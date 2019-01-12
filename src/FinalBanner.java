3724a9e137b0cb38a84030fc323a7dee75be0283
Hemaprasanthi Mutyala <42854519+Hemaprasanthi@users.noreply.github.com>
2019-01-11 21:14:29 -0800

    Add files via upload

---
+++ src/FinalBanner.java
@@ -0,0 +1,14 @@
+import greenfoot.*;  //(World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+public class FinalBanner extends Actor //this actor(image) is for displaying whether the player has won or lost.
+{
+    public FinalBanner(String image){
+        setImage(image);
+    }
+    
+    /**
+     * Act of FinalBanner
+     */
+    public void act(){}    
+}
