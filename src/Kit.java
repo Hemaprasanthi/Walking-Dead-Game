2872fc5987d4e7c11c59106c3dd95910e8a43023
Hemaprasanthi Mutyala <42854519+Hemaprasanthi@users.noreply.github.com>
2019-01-12 23:09:00 -0800

    Add files via upload

---
+++ src/Kit.java
@@ -0,0 +1,16 @@
+import greenfoot.*;  //(World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Kit extends Actor
+{
+    public Kit(){
+        GreenfootImage image1 = new GreenfootImage("firstaid.png");
+        image1.scale(image1.getWidth() - 10, image1.getHeight() - 10);
+        setImage(image1);
+    }
+    
+    /**
+     * Act of Kit
+     */
+    
+    public void act(){}    
+}
