2f618f02768a43527fac41aa72fdb38ad8b0d3a1
Hemaprasanthi Mutyala <42854519+Hemaprasanthi@users.noreply.github.com>
2019-01-14 22:14:41 -0800

    Add files via upload

---
+++ src/LevelOneState.java
@@ -0,0 +1,26 @@
+
+
+
+/**
+ * Write a description of class LevelOneState here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import greenfoot.*; 
+public class LevelOneState implements LevelState 
+{
+    // instance variables - replace the example below with your own
+    
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void doAction(World world)
+    {
+                
+    }
+}
