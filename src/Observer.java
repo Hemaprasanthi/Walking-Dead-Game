7f0dc2dab25e4f9eaa98d7241d4aec4ec7dc0491
Hemaprasanthi Mutyala <42854519+Hemaprasanthi@users.noreply.github.com>
2019-01-14 22:16:16 -0800

    Add files via upload

---
+++ src/Observer.java
@@ -0,0 +1,10 @@
+/** 
+ * Write a description of class Observer here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Observer  
+{
+ void update(String item, Message m);
+}
