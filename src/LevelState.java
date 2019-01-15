1a25359212e455ac04cde86d4f771cb3a68a48cb
Hemaprasanthi Mutyala <42854519+Hemaprasanthi@users.noreply.github.com>
2019-01-14 22:14:53 -0800

    Add files via upload

---
+++ src/LevelState.java
@@ -0,0 +1,18 @@
+/**
+ * Write a description of class LevelState here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import greenfoot.*;
+public interface LevelState  
+{
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void doAction(World world);
+
+}
